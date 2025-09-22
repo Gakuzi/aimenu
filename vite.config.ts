806a11f45a8b9642cd58c943075a182118c8c0b1
Evgeny <eklimov84@gmail.com>
2025-09-22 20:17:18 +0300

    Непонятные мне изменения

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/aimenu/',
       plugins: [react()],
       build: {
         rollupOptions: {
@@ -18,10 +17,6 @@ export default defineConfig(({ mode }) => {
             }
           }
         }
-      },
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
